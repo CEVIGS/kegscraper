954359c512cf0ad1c66020329d39880b68880321
bpph <mfeise_c21@kegs.org.uk>
2026-01-15 16:54:57 +0000

    feat: oliver session class

--- src/kegscraper/oliver/session.py
+++ src/kegscraper/oliver/session.py
@@ -1,27 +1,37 @@
 import pprint
 import httpx
+
 from bs4 import BeautifulSoup
+from dataclasses import dataclass, field
+
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 
 from .utils import api_fetch
 from kegscraper.util.commons import eval_inputs, consume_json
 
+@dataclass
+class Session:
+    rq: httpx.Client
+
+    def __post_init__(self):
+        resp = self.rq.get("https://kegs.oliverasp.co.uk/library/home/api/borrower/details")
+        # TODO: remove this when we are 100% that auth is working.
+        pprint.pp(resp.json())
+
+
 def login(username: str, password: str):
-    client = httpx.Client()
-    client.headers = {
+    client = httpx.Client(headers={
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-    }
+    })
 
     resp = client.get("https://kegs.oliverasp.co.uk/library/home/news")
     soup = BeautifulSoup(resp.text, "html.parser")
     inputs = eval_inputs(soup)
-    pprint.pp(inputs) 
+
     resp = client.get("https://kegs.oliverasp.co.uk/library/home/news",
                       params=inputs)
     soup = BeautifulSoup(resp.text, "html.parser")
-    print(soup.prettify())
-
     search_str = "LOGIN_DATA = {"
     data = None
     for script in soup.find_all("script", {"type": "text/javascript"}):
@@ -40,11 +50,8 @@ def login(username: str, password: str):
     pkm: str = login_dialog["publicKeyModulus"]
     pke: str = login_dialog["publicKeyExponent"]
     sid: str = login_dialog["sessionId"]
-    # sid = client.cookies.get("JSESSIONID")
     
     jpass = oliver_rsa(pkm, pke, sid, password).hex()
-    print(inputs)
-
     resp = client.post("https://kegs.oliverasp.co.uk/library/ClientLookup", data={
         "corporation": corporation,
         "afterLoginUuid": "",
@@ -53,11 +60,11 @@ def login(username: str, password: str):
         "j_password": jpass,
         "j_username": username
     })
-    print(resp)
 
-    resp = client.get("https://kegs.oliverasp.co.uk/library/home/api/borrower/details")
-    print(resp.content)
-    print(client.cookies)
+    
+    return Session(
+        rq=client
+    )
 
 
 def oliver_rsa(pkm: str, pke: str, sid: str, password: str) -> bytes:
