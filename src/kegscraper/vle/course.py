877b9d6e09c8622809a065378b82a13aa769bd55
bpph <mfeise_c21@kegs.org.uk>
2026-01-19 18:43:26 +0000

    chore: async course (Untested)

--- src/kegscraper/vle/course.py
+++ src/kegscraper/vle/course.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import warnings
 from dataclasses import dataclass, field
 from datetime import datetime
+from typing_extensions import Optional
 
 from bs4 import BeautifulSoup
 
@@ -12,22 +13,24 @@ from ..util import exceptions
 
 @dataclass
 class Course:
-    id: int = None
-    display_name: str = None  # == fullname == fullnamedisplay
-    name: str = None  # == shortname
-    summary: BeautifulSoup = None  # summaryformat is always == 1 for me. maybe that affects this
+    _sess: session.Session
 
-    start_time: datetime = None
+    id: Optional[int] = None
+    display_name: Optional[str] = None  # == fullname == fullnamedisplay
+    name: Optional[str] = None  # == shortname
+    summary: Optional[BeautifulSoup] = (
+        None  # summaryformat is always == 1 for me. maybe that affects this
+    )
+
+    start_time: Optional[datetime] = None
     end_time: datetime | None = None  # set to none if the value is 0
-    access_time: datetime = None
+    access_time: Optional[datetime] = None
 
-    category: coursecategory.CourseCategory = None
+    category: Optional[coursecategory.CourseCategory] = None
 
-    image: str = field(default=None, repr=False)
+    image: Optional[str] = field(default=None, repr=False)
 
-    hidden: bool = None
-
-    _sess: session.Session = None
+    hidden: Optional[bool] = None
 
     """
     Should these attrs be ignored?:
@@ -40,10 +43,12 @@ class Course:
     """
 
     @classmethod
-    def from_json(cls, data: dict[str, str | bool | int], sess: session.Session = None):
+    def from_json(cls, data: dict, sess: session.Session):
         if data["fullname"] != data["fullnamedisplay"]:
-            warnings.warn(f"Please report to github, fullname != fullnamedisplay: {data}",
-                          category=exceptions.UnimplementedWarning)
+            warnings.warn(
+                f"Please report to github, fullname != fullnamedisplay: {data}",
+                category=exceptions.UnimplementedWarning,
+            )
 
         return cls(
             _sess=sess,
@@ -52,11 +57,15 @@ class Course:
             name=data["shortname"],
             summary=BeautifulSoup(data["summary"], "html.parser"),
             start_time=datetime.fromtimestamp(data["startdate"]),
-            end_time=datetime.fromtimestamp(data["enddate"]) if data.get("endate", 0) != 0 else None,
+            end_time=(
+                datetime.fromtimestamp(data["enddate"])
+                if data.get("endate", 0) != 0
+                else None
+            ),
             access_time=datetime.fromtimestamp(data["timeaccess"]),
             image=data["courseimage"],
             hidden=data["hidden"],
-            category=coursecategory.CourseCategory(_sess=sess,
-                                                   name=data["coursecategory"])
-
+            category=coursecategory.CourseCategory(
+                _sess=sess, name=data["coursecategory"]
+            ),
         )
--- src/kegscraper/vle/coursecategory.py
+++ src/kegscraper/vle/coursecategory.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing_extensions import Optional
 
 from . import session
 
+
 @dataclass
 class CourseCategory:
-    name: str = None # e.g. 'Chemistry Y9-11'
+    _sess: session.Session
+    name: Optional[str] = None  # e.g. 'Chemistry Y9-11'
 
-    _sess: session.Session = None
