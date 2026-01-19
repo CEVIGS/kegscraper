af3083043573e069503ae4dbe34ef6657b75a59e
bpph <mfeise_c21@kegs.org.uk>
2026-01-19 18:44:39 +0000

    chore: async calendar  (Untested)

--- src/kegscraper/vle/calendar.py
+++ src/kegscraper/vle/calendar.py
@@ -4,7 +4,7 @@ import warnings
 from dataclasses import dataclass, field
 from datetime import datetime
 from urllib.parse import urlparse, parse_qs
-from typing_extensions import Any, Self
+from typing_extensions import Any, Self, Optional
 
 import dateparser
 from bs4 import PageElement, BeautifulSoup
@@ -15,14 +15,14 @@ from ..util import commons, exceptions
 
 @dataclass
 class Event:
-    title: str = None
-    description: str = None
-    location: str = None
-    type: str = None
+    _sess: session.Session
 
-    date: datetime = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    location: Optional[str] = None
+    type: Optional[str] = None
 
-    _sess: session.Session = None
+    date: Optional[datetime] = None
 
 
 @dataclass
@@ -31,6 +31,5 @@ class Calendar:
     An instance of a calendar widget that KEGSNet can give
     """
 
+    _sess: session.Session
     events: list[Event] = field(default_factory=list)
-
-    _sess: session.Session = None
